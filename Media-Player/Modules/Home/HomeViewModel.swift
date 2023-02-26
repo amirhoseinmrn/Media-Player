0fbabe48d642962b317bab431a421fe58263292a
amirhoseinmrn <amirhoseinmrn1@gmail.com>
2023-02-27 01:54:35 +0400

    Add home view model

---
+++ Media-Player/Modules/Home/HomeViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewModel.swift
+//  Media-Player
+//
+//  Created by amir on 2/26/23.
+//
+
+import Foundation
+
+class HomeViewModel: BaseViewModel,
+                     ListVideoPO {
+    var listVideoService: ListVideoServiceProtocol
+    var listVideoResponse: Paginable<VideoModel>?
+    var listVideoChangeHandler: ((BaseViewModelChange) -> Void)?
+    
+    init(listVideoService: ListVideoServiceProtocol = ListVideoService()) {
+        self.listVideoService = listVideoService
+    }
+}
+
+extension HomeViewModel {
+    func getListVideo(isRefresh: Bool = false) {
+        var page = (listVideoResponse?.page ?? 0) + 1
+        if isRefresh { page = 1 }
+        let perPage = 10
+        let request = ListVideoRequest(page: page, perPage: perPage)
+        listVideo(request: request, isRefresh: true)
+    }
+}
