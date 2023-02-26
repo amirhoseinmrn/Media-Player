63a296c76b0be0549676fe06f100a9fe71afa546
amirhoseinmrn <amirhoseinmrn1@gmail.com>
2023-02-27 01:50:01 +0400

    Add empty message extension to tableview

--- Media-Player/Commons/Extensions/Extensions.swift
+++ Media-Player/Commons/Extensions/Extensions.swift
@@ -6,9 +6,30 @@
 //
 
 import Foundation
+import UIKit
 
 extension NSObject {
     @objc static var nameOfClass: String {
         return NSStringFromClass(self).components(separatedBy: ".").last!
+    }
+}
+
+extension UITableView {
+    func setEmptyMessage(_ message: String) {
+        let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
+        messageLabel.text = message
+        messageLabel.textColor = .secondaryLabel
+        messageLabel.numberOfLines = 0
+        messageLabel.textAlignment = .center
+        messageLabel.font = UIFont.preferredFont(forTextStyle: .subheadline)
+        messageLabel.sizeToFit()
+
+        self.backgroundView = messageLabel
+        self.separatorStyle = .none
+    }
+
+    func restore() {
+        self.backgroundView = nil
+        self.separatorStyle = .singleLine
     }
 }
