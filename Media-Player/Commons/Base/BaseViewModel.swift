3f0ae8ef6b986006e72730851cbb4b99ccb88e79
amirhoseinmrn <amirhoseinmrn1@gmail.com>
2023-02-26 16:47:44 +0400

    Add base view model

---
+++ Media-Player/Commons/Base/BaseViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  BaseViewModel.swift
+//
+//  Created by Amir on 11/6/1399 AP.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+enum BaseViewModelChange {
+    case didSuccess
+    case didError(Error)
+    case didChangeNetworkActivityStatus(Bool)
+}
+
+public class BaseViewModel {
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate   
+}
