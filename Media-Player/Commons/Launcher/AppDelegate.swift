dd58900913f8f244164049945c88924243f194b9
amirhoseinmrn <amirhoseinmrn1@gmail.com>
2023-02-26 17:26:29 +0400

    Remove main storyboard

--- Media-Player/Commons/Launcher/AppDelegate.swift
+++ Media-Player/Commons/Launcher/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        setupKeyboard()
         return true
+    }
+    
+    func setupKeyboard() {
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.keyboardDistanceFromTextField = 80
     }
 
     // MARK: UISceneSession Lifecycle
