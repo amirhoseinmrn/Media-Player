952b630d2f8f271a8b52502ce32b72729516856b
amirhoseinmrn <amirhoseinmrn1@gmail.com>
2023-02-26 16:48:44 +0400

    Add styles enumeration

---
+++ Media-Player/Commons/Style/Styles.swift
@@ -0,0 +1,13 @@
+//
+//  Style.swift
+//  Media-Player
+//
+//  Created by amir on 2/26/23.
+//
+
+import Foundation
+
+enum BorderStyles: CGFloat {
+    case defaultCornerRadius = 8
+    case defaultBorderWidth = 1
+}
